3778524e3462d085bcfe4e761d405a89bea3ea6e
yoann.basley <yoann.basley@easyteam.fr>
2014-01-09 20:11:30 +0100

    Ajout du menu

---
+++ Assets/Scripts/Menu/Menu_Specifique.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+var son : AudioClip;
+var levelSuivant : String = "Quitter";
+
+function OnMouseUp() { 
+     audio.PlayOneShot(son);
+     if (levelSuivant == "Quitter")
+         Application.Quit();
+     else
+         Application.LoadLevel(levelSuivant);
+}
+
+@script RequireComponent (AudioSource)
