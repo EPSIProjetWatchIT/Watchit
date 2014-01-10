10214f4db4d0b510b1977bd1d6ad6e2abe601159
Nem0oo <gcourtot@gmail.com>
2014-01-10 16:10:45 +0100

    Projet Normallement PROPRE

---
+++ Assets/Scripts/Personnage/Collisions.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Collisions : MonoBehaviour {
+
+	private Perso personage;
+	private static SupprimeEffet viragePrecedent = null;
+	// Use this for initialization
+	void Start () {
+		personage = gameObject.GetComponent ("Perso") as Perso;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "lifeChanger")
+		{
+			LifeChanges life = other.gameObject.GetComponent("LifeChanges") as LifeChanges;
+			personage.AltereVie (life.variation);
+		}
+
+		if (other.gameObject.tag == "Droite") 
+		{
+			SupprimeEffet virage = other.gameObject.GetComponent ("SupprimeEffet") as SupprimeEffet;
+			if (!virage.traverse) 
+			{
+				transform.Rotate (Vector3.up * 45f);
+				transform.position = new Vector3(other.transform.parent.position.x,transform.position.y,other.transform.parent.position.z);
+				if(viragePrecedent != null)
+					viragePrecedent.traverse = false;
+				viragePrecedent = virage;
+				virage.traverse = true;
+			}
+		}
+		else
+		{
+			if (other.gameObject.tag == "Gauche") {
+				SupprimeEffet virage = other.gameObject.GetComponent ("SupprimeEffet") as SupprimeEffet;
+				if (!virage.traverse)
+				{
+					transform.Rotate (Vector3.up * -45f);
+					transform.position = new Vector3(other.transform.parent.position.x,transform.position.y,other.transform.parent.position.z);
+					if(viragePrecedent != null)
+						viragePrecedent.traverse = false;
+					viragePrecedent = virage;
+					virage.traverse = true;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Personnage/Sautillement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sautillement : MonoBehaviour {
+
+	private const float HAUTEURMAX = 0.002f;
+	//private const float VITESSESAUTILLEMENT = 0.004f;
+	private const float VITESSESAUTILLEMENT = 3f;
+	private Vector3 Saut =new Vector3 (0f, 1f, 0f);
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Sautillement
+		if((transform.localPosition.y > HAUTEURMAX) || (transform.localPosition.y < 0 ))
+			Saut = Saut * -1f;
+
+		transform.Translate(Saut * VITESSESAUTILLEMENT * Time.deltaTime);
+	}
+}
