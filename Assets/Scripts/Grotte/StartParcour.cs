64eb68779eeec4573a987c8d5933410403b645ae
Nem0oo <gcourtot@gmail.com>
2014-01-10 11:10:07 +0100

    Affichage parcour segmenté pour améliorer les performances

---
+++ Assets/Scripts/Grotte/StartParcour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartParcour : MonoBehaviour {
+
+	public GameObject parcour1;
+	public GameObject parcour2;
+
+	// Use this for initialization
+	void Start () {
+		parcour1 = GameObject.Find("Parcour1");
+		parcour2 = GameObject.Find("Parcour2");
+
+		parcour1.SetActive (true);
+		parcour2.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
