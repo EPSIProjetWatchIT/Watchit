8d6a10908a2ec01868eebcd541d769ee559bbcda
Nem0oo <gcourtot@gmail.com>
2014-01-13 13:34:22 +0100

    Animation des bras et Fichier pour le mode tuto et dificulte

---
+++ Assets/Scripts/Fichiers.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Fichiers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	 public static int getDifficulte()
+	{
+		int value = 0;
+		StreamReader file = new StreamReader ("difficulte.txt");
+		value = int.Parse(file.ReadLine());
+		file.Close ();
+		return value;
+	}
+
+	public static void setDifficulte(int value=0)
+	{
+		StreamWriter file = new StreamWriter ("difficulte.txt");
+		file.WriteLine (value.ToString ());
+		file.Close ();
+	}
+
+	public static void setTutoMode(int value=0)
+	{
+		StreamWriter file = new StreamWriter ("tutoMode.txt");
+		file.WriteLine (value.ToString ());
+		file.Close ();
+	}
+
+	public static int getTutoMode()
+	{
+		int value = 0;
+		StreamReader file = new StreamReader ("tutoMode.txt");
+		value = int.Parse(file.ReadLine());
+		file.Close ();
+		return value;
+	}
+}
--- Assets/Scripts/Personnage/Sautillement.cs
+++ Assets/Scripts/Personnage/Sautillement.cs
@@ -13,13 +13,25 @@ public class Sautillement : MonoBehaviour {
 	public AudioSource footstep;
 
 
+	private GameObject brasGauche;
+	private GameObject brasDroit;
+	private float VitesseBras = 15f;
+	private bool BGForward = true;
+	private bool BDForward = false;
+	private float LimiteMax = 50f;
+	private float LimiteMin = -30f;
+
+
 	// Use this for initialization
 	void Start () {
 		HAUTEUR = HAUTEURMIN;
+		brasDroit = GameObject.Find ("DroitEpaule");
+		brasGauche = GameObject.Find ("GaucheEpaule");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//SAUTILLEMENT
 		//On est en dessous du sol ou au sol, on rebondit
 		if ((HAUTEUR <= HAUTEURMIN && down) ||(HAUTEUR >= HAUTEURMAX && up))
 		{
@@ -36,9 +48,35 @@ public class Sautillement : MonoBehaviour {
 			down = !down;
 			up = !up;
 		}
-
-
 		transform.Translate (Vector3.up * VITESSE *Time.deltaTime);
 		HAUTEUR = transform.localPosition.y;
+
+		//BRAS
+		if ((brasDroit.transform.localRotation.x >= LimiteMax && BDForward) || (brasDroit.transform.localRotation.x <= LimiteMin && !BDForward)) 
+		{
+			VitesseBras = -VitesseBras;
+			BDForward = !BDForward;
+			BGForward = !BGForward;
+		}
+		brasDroit.transform.Rotate (Vector3.up * -VitesseBras);// * Time.deltaTime);
+		brasGauche.transform.Rotate (Vector3.up * VitesseBras);// * Time.deltaTime);
+
+
 	}
+
+	/*public void BrasDroit(float degres)
+	{
+
+		brasDroit.transform.Rotate (0f, 0f, -brasDroit.transform.rotation.z);
+		brasDroit.transform.Rotate (0f, 0f, degres);
+	}
+	
+	public void BrasGauche(float degres)
+	{
+		GameObject brasGauche = GameObject.Find ("GaucheEpaule");
+		brasGauche.transform.Rotate (0f, 0f, -brasGauche.transform.rotation.z);
+		brasGauche.transform.Rotate (0f, 0f, degres);
+	}
+*/
+
 }
