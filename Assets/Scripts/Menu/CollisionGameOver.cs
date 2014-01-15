33d311e0ab5b63c4283a06955fac02c5a844178a
Nem0oo <gcourtot@gmail.com>
2014-01-15 12:28:54 +0100

    Correction probleme Menu GameOver

---
+++ Assets/Scripts/Menu/CollisionGameOver.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionGameOver : MonoBehaviour {
+
+	private float delais = 3f;
+	private float selectionDepuis = 0f;
+	private bool selection = false;
+	private string scene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selection) 
+		{
+			selectionDepuis += Time.deltaTime;
+			if(selectionDepuis >= delais)
+				Application.LoadLevel(scene);
+		}
+		else 
+		{
+			selectionDepuis = 0f;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		selection = true;
+		if (other.gameObject.tag == "Grotte") 
+		{
+			scene = "Scene_grotte";
+		}
+		else 
+		{
+			scene = "MenuAvecMinion";
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+			selection = false;
+	}
+}
