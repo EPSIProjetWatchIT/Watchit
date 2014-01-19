93d3cb319d3e70a5b9f23a98349e03609603e552
Nem0oo <gcourtot@gmail.com>
2014-01-19 17:48:56 +0100

    Cinématique 2 passage

---
+++ Assets/Kinect/Script/Kinect/Gesture/PassCine2.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassCine2 : MonoBehaviour {
+	public SkeletonWrapper sw;
+	[HideInInspector]
+	public Vector3[,] bonePos;
+
+
+
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		bonePos = new Vector3[2,(int)Kinect.NuiSkeletonPositionIndex.Count];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sw.pollSkeleton ()) {
+		
+			Vector3 shoulderLeftGo = sw.bonePos [0, (int)Kinect.NuiSkeletonPositionIndex.ShoulderLeft];
+			
+	
+			print ("y Gauche : " + shoulderLeftGo.y.ToString ());
+	
+					
+					
+			if (MenuDetection ()) {
+				Application.LoadLevel("Scene_Chateau_2");
+			}
+
+		}
+	}
+	
+	
+	bool MenuDetection()
+	{
+		bool pass=false;
+		Vector3 handLeft = sw.bonePos [0, (int)Kinect.NuiSkeletonPositionIndex.HandLeft];
+
+		Vector3 shoulderLeft = sw.bonePos [0, (int)Kinect.NuiSkeletonPositionIndex.ShoulderRight];
+		
+
+		
+		print("handLeft Y: "+handLeft.y.ToString());
+		print ("handleft X : " + handLeft.x.ToString ());
+		
+		// Left and right hands below hip  &&    left hand 0.3 to left of center hip
+		if ((handLeft.y > shoulderLeft.y)) 
+			pass=true;
+
+			return pass;
+
+	}
+}
---
+++ Assets/Scenes/Scene_Cinematiques/Scripts/FinCine2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinCine2 : MonoBehaviour {
+
+
+	private float TempsDebut=0f;
+	public float delais;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TempsDebut += Time.deltaTime;
+		if (TempsDebut >= delais)
+		{
+			Application.LoadLevel("Scene_Chateau_2");
+		}
+
+
+	
+	}
+}
